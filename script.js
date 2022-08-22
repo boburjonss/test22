896483d799b2a82a08a7e250bb23e46f44b18565
BOBURJON <sahodullaevboburzon@gmail.com>
2022-08-22 08:49:47 +0500

    test22

---
+++ script.js
@@ -0,0 +1,206 @@
+"user "
+
+
+// const obj = {
+//     name: 'div',
+//     userName: 'kattaQuti',
+//     age: 2,
+//     colors: {
+//         bg: 'crimson',
+//         color: 'red',
+//     }
+// }
+
+// console.log(obj);
+// delete obj.userName
+// console.log(obj);
+// console.log(obj.name);
+
+
+
+// sicl orqali obj ni tartiblash
+
+// for( let key in obj){
+//     console.log(key);
+//     if(typeof obj[key] === "Object"){
+//         for(let i in obj [key]){
+//             console.log(`'property' ${i} 'value' ${obj[key][i]}`);
+//         }
+//     }else{
+//         console.log(`'property' ${key} 'value' ${obj[key]}`);
+//     }
+// }
+// console.log(obj);
+
+
+
+
+
+
+
+
+
+// if1
+// let a = +prompt('raqam kirit !!!');
+
+// if(a > 0){
+//     alert(++a);
+// }
+// else{
+//     alert(a)
+// }
+
+
+
+// if2
+// let a = +prompt('raqam kirit !!!');
+
+
+// if(a > 0){
+//     alert(++a);
+// }
+// else{
+//     alert(a-2)
+// }
+
+
+
+
+
+// if3
+
+// let a = +prompt('raqam kirit !!!');
+
+
+// if(a > 0){
+//     alert(++a);
+// }
+// else if( a == 0){
+//     alert(10)
+// }
+// else{
+//     alert(a-2)
+// }
+
+
+
+// if-4
+// let a = +prompt('1-son'),
+// b=+prompt('2-son'),
+// c=+prompt('3-son');
+
+
+// if(a>0 && b>0 &&c>0){
+//     console.log("3ta musbat son");
+// }
+// else if((a>0 && b>0) || (b>0 && c>0 ) ||(a>0 && c>0 )) {
+//     console.log("2ta musbat son");
+// }
+// else if((a>0 && b<0 && c<0) || (a<0 && b>0 && c<0) || (a<0 && b<0 && c>0)){
+//     console.log("1ta musbat son");
+// }
+// else{
+//     console.log("musbat son yoq");
+// }
+
+
+// if-5
+
+// let a = +prompt('1-son'),
+// b=+prompt('2-son'),
+// c=+prompt('3-son');
+
+
+// if(a>0 && b>0 &&c>0){
+//     console.log("3ta musbat son va 0ta  manfiy");
+// }
+// else if((a>0 && b>0) || (b>0 && c>0 ) ||(a>0 && c>0 )) {
+//     console.log("2ta musbat son va 1ta manfiy");
+// }
+// else if((a>0 && b<0 && c<0) || (a<0 && b>0 && c<0) || (a<0 && b<0 && c>0)){
+//     console.log("1ta musbat son va 2ta manfiy");
+// }
+// else{
+//     console.log("3ta manfiy va musbat son yoq");
+// }
+
+
+
+
+// if-6
+
+// let a = +prompt('son'),
+// b = +prompt('son');
+
+
+// if(a > b ){
+//     console.log(a);
+// }
+// else if(a < b){
+//     console.log(b);
+// }
+
+
+
+
+
+// if-7
+
+
+// let a = +prompt('son1'),
+// b = +prompt('son2');
+
+// if(a > b){
+//     console.log(b);
+// }else if(b > a){
+//     console.log(a);
+// }
+// else {
+//     console.log('qiymatlar teng');
+// }
+
+
+
+// if-8
+
+// let a = +prompt('son1'),
+// b = +prompt('son2');
+
+// if(a < b || a > b){
+//     console.log(a,b);
+// }
+
+// if-9
+
+
+// let a = +prompt('son1'),
+// b = +prompt('son2');
+
+// if (a) {
+    
+// }
+
+
+
+
+
+// 12
+
+let a = +prompt('son1'),
+b = +prompt('son2'),
+c = +prompt('son3');
+
+if(a > b && b>c &&b>c){
+    console.log("3-chsi kichik");
+}
+else if(b > a && a>c &&b>c) {
+    console.log("2-chsi kichik");
+}
+else if(c > a && b>a &&c>a){
+    console.log("1");
+}
+else{
+    console.log("3ta manfiy va musbat son yoq");
+}
+
+
